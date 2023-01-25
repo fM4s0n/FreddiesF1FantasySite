df8987f9b387bd43c25a160967881da89bcc85c9
fM4s0n <115559911+fM4s0n@users.noreply.github.com>
2023-01-25 09:26:22 +0000

    fixed manage account!

--- DataAccess/Database/SqlDataAccess.cs
+++ DataAccess/Database/SqlDataAccess.cs
@@ -9,7 +9,7 @@ public class SqlDataAccess : ISqlDataAccess
 {
     private readonly IConfiguration _config;
 
-    public string ConnectionStringName { get; set; } = "Default";
+    public string ConnectionStringName { get; set; } = "Laptop";
 
     public SqlDataAccess (IConfiguration config)
     {
