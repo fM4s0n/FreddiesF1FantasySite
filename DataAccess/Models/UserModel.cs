c0d37388c5498eeeecf192b01f2fbec9988a5007
fM4s0n <115559911+fM4s0n@users.noreply.github.com>
2023-01-26 09:11:44 +0000

    added account levels

--- DataAccess/Models/UserModel.cs
+++ DataAccess/Models/UserModel.cs
@@ -5,13 +5,29 @@ namespace DataAccess.Models;
 public class UserModel
 {
     public int UserID { get; set; }
+
     public string Username { get; set; } = "";
+
     public string Password { get; set; } = "";
+
     public string FullName { get; set; } = "";
+
+    public AccountLevels AccountLevel { get; set; } = AccountLevels.Standard;
+
     public string FavouriteDriverName { get; set; } = "";
+
     public string FavouriteDriverId { get; set; } = "";
+
     public string FavouriteConstructorName { get; set; } = "";
+
     public string FavouriteConstructorId { get; set; } = "";
 
     //public List<FantasyF1LeagueMembership> FantasyLeagueMemberships { get; set; }
+}
+
+public enum AccountLevels
+{
+    Standard,
+    Pro,
+    Administrator
 }
