2ff7084429c15cb5273310a41471196c18cf1593
fM4s0n <115559911+fM4s0n@users.noreply.github.com>
2023-01-27 13:07:06 +0000

    carried on with create league page

--- DataAccess/Models/Fantasy/FantasyLeagueModel.cs
+++ DataAccess/Models/Fantasy/FantasyLeagueModel.cs
@@ -1,11 +1,19 @@
-﻿namespace DataAccess.Models.Fantasy;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DataAccess.Models.Fantasy;
 
 public class FantasyLeagueModel
 {
     public int LeagueId { get; set; }
 
+    [Required]
+    [StringLength(maximumLength: 30, ErrorMessage ="Name is too long, max length is 30 characters")]
+    [MinLength(length: 5, ErrorMessage = "Name is too short, minimum length is 5 characters")]
     public string LeagueName { get; set; } = string.Empty;
 
+    [Required]
+    [StringLength(maximumLength: 30, ErrorMessage = "Password is too long, max length is 30 characters")]
+    [MinLength(length: 5, ErrorMessage = "Password is too short, minimum length is 5 characters")]
     public string LeaguePassword { get; set; } = string.Empty;
 
     public List<FantasyTeamModel>? LeagueTeamsIds { get; set; }
