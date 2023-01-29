7b6e8ed712b880f1959e7bef8af35c57c2e0015f
fM4s0n <115559911+fM4s0n@users.noreply.github.com>
2023-01-29 17:18:17 +0000

    couple more bits done

--- DataAccess/Database/TeamData.cs
+++ DataAccess/Database/TeamData.cs
@@ -4,5 +4,49 @@ namespace DataAccess.Database;
 
 public class TeamData
 {
+    private readonly ISqlDataAccess _db;
 
+    public TeamData (ISqlDataAccess db)
+    {
+        _db = db;
+    }
+
+    public Task<List<FantasyTeamModel>> GetAllLeagues ()
+    {
+        string sql = "SELECT * FROM tblTeam";
+
+        return _db.LoadData<FantasyTeamModel, dynamic>(sql, new { });
+    }
+
+    //might need chnage this to get by user Id then list for user to pick
+    public Task<List<FantasyTeamModel>> GetLeague (int teamId)
+    {
+        string sql = "SELECT * FROM tblTeam WHERE TeamId = @teamId";
+
+        return _db.LoadData<FantasyTeamModel, dynamic>(sql, new { });
+    }
+
+    public Task<List<FantasyTeamModel>> GetMostRecentLeague ()
+    {
+        string sql = "SELECT TOP 1 * FROM tblTeam ORDER BY TeamId DESC";
+
+        return _db.LoadData<FantasyTeamModel, dynamic>(sql, new { });
+    }
+
+
+    public Task InsertLeague (FantasyTeamModel team)
+    {
+        string sql = @"INSERT INTO dbo.tblTeam 
+                       VALUES (@, @, @, @, @, @, @);";
+
+        return _db.SaveData(sql, team);
+    }
+
+    public Task UpdateLeagueDetails (FantasyTeamModel team)
+    {
+        string sql = @"UPDATE dbo.tblTeam SET TeamName = @team.LeagueName,
+                       WHERE TeamId = @team.TeamId";
+
+        return _db.SaveData(sql, team);
+    }
 }
